ee197cfa12b135a806d0d365568803dc020c4060
shenshuyang <shenshuyang@moonshot.cn>
2025-01-13 00:18:14 +0800

    feat: ac automation for partial match

--- src/matcher/aca.rs
+++ src/matcher/aca.rs
@@ -1,11 +1,128 @@
+use std::{borrow::Borrow, collections::VecDeque};
+
 use crate::Tpair;
 
 pub fn new(pairs: Vec<Tpair>) -> Acam {
-  Acam {}
+    let mut acam = Acam {
+        cnt: 0,
+        ncnt: vec![0],
+        fail: vec![0],
+        trie: vec![[0; 256]],
+        exist: vec![0],
+        addup: vec![0],
+    };
+    pairs
+        .into_iter()
+        .for_each(|x| acam.insert(x.smark.borrow()));
+    return acam;
 }
 
-pub struct Acam {}
+pub struct Acam {
+    cnt: usize,
+    ncnt: Vec<usize>,
+    fail: Vec<usize>,
+    trie: Vec<[usize; 256]>,
+    exist: Vec<usize>,
+    addup: Vec<usize>,
+}
 
 pub fn do_match(acam: &Acam, chunk: &str) -> (usize, bool) {
-  (0, false)
+    acam.query(chunk)
+}
+
+impl Acam {
+    fn insert(&mut self, pat: &str) {
+        let mut p = 0;
+        let mut span = 0;
+        let mut spawn = 0;
+        pat.bytes().into_iter().for_each(|x| {
+            if self.trie[p][x as usize] == 0 {
+                spawn += 1;
+                self.cnt += 1;
+                self.fail.push(0);
+                self.ncnt.push(0);
+                self.trie.push([0; 256]);
+                self.exist.push(0);
+                self.addup.push(0);
+                self.trie[p][x as usize] = self.cnt;
+            } else {
+                span += 1;
+            }
+            self.ncnt[p] += 1;
+            p = self.trie[p][x as usize];
+        });
+        self.addup[p - spawn] = span;
+        self.exist[p] = pat.len();
+    }
+
+    fn build(&mut self) {
+        self.exist
+            .clone()
+            .into_iter()
+            .zip(self.addup.iter_mut())
+            .fold(0, |acc, (e, aref)| {
+                let bak = *aref;
+                *aref = acc + e - bak;
+                acc + e - bak
+            });
+
+        let mut q = VecDeque::new();
+        self.trie[0].into_iter().for_each(|x| {
+            if x != 0 {
+                q.push_back(x);
+            }
+        });
+
+        while !q.is_empty() {
+            let k = q.pop_front().unwrap();
+            self.trie[k]
+                .into_iter()
+                .enumerate()
+                .for_each(|(i, x)| match x {
+                    0 => self.trie[k][i] = self.trie[self.fail[k]][i],
+                    _ => {
+                        self.fail[x] = self.trie[self.fail[k]][i];
+                        q.push_back(x);
+                    }
+                });
+        }
+    }
+
+    fn query(&self, chunk: &str) -> (usize, bool) {
+        let mut t = 0;
+        for (i, x) in chunk.bytes().enumerate() {
+            t = self.trie[t][x as usize];
+            while t != 0 && self.ncnt[t] == 0 && self.exist[t] == 0 {
+                t = self.fail[t];
+            }
+            if self.exist[t] != 0 {
+                return (i - self.exist[t] + 1, true);
+            }
+        }
+        (chunk.len() + self.addup[t] - t, false)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::new_pair;
+    #[test]
+    fn acam_functionality() {
+        let mut acam = new(vec![
+            new_pair("oof", "bar"),
+            new_pair("我试试", "你试试"),
+            new_pair("helle", "world"),
+            new_pair("我试哥", "你试试"),
+            new_pair("helloo", "world"),
+            new_pair("start", "end"),
+        ]);
+        acam.build();
+        let (idx, ok) = acam.query("ohellooabc");
+        println!("{}, {}", idx, ok);
+        println!("ncnt : {:?}, len: {}", acam.ncnt, acam.ncnt.len());
+        println!("fail : {:?}", acam.fail);
+        println!("exist: {:?}", acam.exist);
+        println!("addup: {:?}, len: {}", acam.addup, acam.addup.len());
+    }
 }
--- src/processor/tacam.rs
+++ src/processor/tacam.rs
@@ -1,70 +1,72 @@
 use std::collections::HashMap;
 
+use crate::matcher::kmp::Kpair;
+
 pub fn new_tacam_processor(pairs: Vec<Tpair>) -> impl Slob {
-  TacamProcessor {
-    state: State::StateQuest,
-    buffer: String::new(),
-    acam: aca::new(pairs),
-    value: None,
-    kpairs: [kmp::new(""), kmp::new("")],
-    m2value: HashMap::new(),
-    m2kpair: HashMap::new(),
-  }
+    TacamProcessor {
+        state: State::StateQuest,
+        buffer: String::new(),
+        acam: aca::new(pairs),
+        value: None,
+        kpairs: [kmp::new(""), kmp::new("")],
+        m2value: HashMap::new(),
+        m2kpair: HashMap::new(),
+    }
 }
 
 struct TacamProcessor {
-  state: State,
-  buffer: String,
-  acam: aca::Acam,
-  value: Option<String>,
-  kpairs: [kmp::Kpair; 2],
-  m2value: HashMap<String, String>,
-  m2kpair: HashMap<String, kmp::Kpair>,
+    state: State,
+    buffer: String,
+    acam: aca::Acam,
+    value: Option<String>,
+    kpairs: [kmp::Kpair; 2],
+    m2value: HashMap<String, String>,
+    m2kpair: HashMap<String, kmp::Kpair>,
 }
 
 impl Slob for TacamProcessor {
-  fn process(&mut self, chunk: &str) -> Vec<Part> {
-    let mut parts = vec![];
-    self.buffer.push_str(chunk);
-    while {
-      let kpair = &self.kpairs[self.state as usize];
-      let (idx, encore) = match self.state {
-        State::StateQuest => aca::do_match(&self.acam, &self.buffer),
-        State::StateMatch => kmp::do_match(kpair, &self.buffer),
-        _ => panic!("Invalid state: {}", self.state),
-      };
-      match idx {
-        0 => {}
-        _ => {
-          parts.push(Part {
-            content: self.buffer[..idx].to_owned(),
-            state: self.state,
-            value: None,
-          });
-        }
-      }
-      match encore {
-        false => {}
-        true => {
-          parts.push(Part {
-            content: "".to_owned(),
-            state: match self.state {
-              State::StateQuest => State::StateSmark,
-              State::StateMatch => State::StateEmark,
-              _ => panic!("Invalid state: {}", self.state),
-            },
-            value: None,
-          });
-          self.state = match self.state {
-            State::StateQuest => State::StateMatch,
-            State::StateMatch => State::StateQuest,
-            _ => panic!("Invalid state: {}", self.state),
-          }
-        }
-      }
-      self.buffer = self.buffer[idx + (tmark.mark.len() * encore as usize)..].to_owned();
-      encore
-    } {}
-    parts
-  }
+    fn process(&mut self, chunk: &str) -> Vec<Part> {
+        let mut parts = vec![];
+        self.buffer.push_str(chunk);
+        while {
+            let kpair = &self.kpairs[self.state as usize];
+            let (idx, encore) = match self.state {
+                State::StateQuest => aca::do_match(&self.acam, &self.buffer),
+                State::StateMatch => kmp::do_match(kpair, &self.buffer),
+                _ => panic!("Invalid state: {}", self.state),
+            };
+            match idx {
+                0 => {}
+                _ => {
+                    parts.push(Part {
+                        content: self.buffer[..idx].to_owned(),
+                        state: self.state,
+                        value: None,
+                    });
+                }
+            }
+            match encore {
+                false => {}
+                true => {
+                    parts.push(Part {
+                        content: "".to_owned(),
+                        state: match self.state {
+                            State::StateQuest => State::StateSmark,
+                            State::StateMatch => State::StateEmark,
+                            _ => panic!("Invalid state: {}", self.state),
+                        },
+                        value: None,
+                    });
+                    self.state = match self.state {
+                        State::StateQuest => State::StateMatch,
+                        State::StateMatch => State::StateQuest,
+                        _ => panic!("Invalid state: {}", self.state),
+                    }
+                }
+            }
+            self.buffer = self.buffer[idx + (kpair.mark.len() * encore as usize)..].to_owned();
+            encore
+        } {}
+        parts
+    }
 }
